72af8143e984eebadc2a8b7299e9fd243cbeb2c2
sacchoprogrammer <sacchoprogram@gmail.com>
2021-07-31 14:57:53 +0600

    In function aliases

--- src/script.ts
+++ src/script.ts
@@ -1,31 +1,18 @@
-// declare string variable player name
-let playerName = "Masrafi";
-playerName = "Asrafull";
-playerName = "Sakib"
+type stringOrNumber = string | number
+type userType = {name:string, age:number}
 
-// Type 'number' is not assignable to type 'string'
-// playerName = 123
-
-// Type 'stirng' in function paramiter
-function getPlayer(playerOne:string, playerTwo:string) {
-    return playerName + playerTwo;
+// userDetials function 
+const userDetials = (id:stringOrNumber, name:string) =>{
+    console.log(`user id is ${id}, name is ${name}`)
 }
 
-// let playerList: string[] array
-let playerList = ["Masrafi", "Asarafull", "Sakib", "Rafiq", "Mostafiz"];
-
-// let playerInfo object declare
-let playerInfo = {
-    name: 'Masrafi Bin Mortuza',
-    age:36,
-    dateOfBirth:1985,
-    isCaptain: true
+// sayHello function
+const sayHello = (user:userType) =>{
+    console.log(`Hello ${user.age > 50 ? "Sir" : "Mr."} ${user.name}`);
 }
 
-// playerInfo.name string type
-// playerInfo.age number type
-// playerInfo.dateOfBirth number type
+// print Hello Mr. shanto
 
-console.log(playerList.push("Bijoy"));
-console.log(getPlayer(" Rafiq "," Mostafiz "));
-console.log(playerName);
+console.log(sayHello({name: 'shanto', age: 26}));
+
+
